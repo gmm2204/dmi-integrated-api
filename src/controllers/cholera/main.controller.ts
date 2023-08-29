72cf4656536c394a991fdb8896039f2baf85b0a8
gmm2204 <gmm2204@cumc.columbia.edu>
2023-08-29 16:20:26 +0300

    * Implemented post method for filter routes for mortality_ncov * Implemented conditional param on routes for filters * Implemented filters queries

--- src/controllers/cholera/main.controller.ts
+++ src/controllers/cholera/main.controller.ts
@@ -1,24 +1,11 @@
 import { Request, Response } from "express";
-import mainRepository from "../../repositories/mortality_ncov/main.repository";
+import mainRepository from "../../repositories/cholera/main.repository";
 
 export default class MainController {
     async getData(req: Request, res: Response) {
         try {
             let dataInstance: any;
             dataInstance = await mainRepository.readData(req.url);
-            res.status(200).send(dataInstance);
-        }
-        catch (err) {
-            res.status(500).send({
-                message: "An error has occured here --> " + req.url
-            });
-        }
-    }
-
-    async postData(req: Request, res: Response) {
-        try {
-            let dataInstance: any;
-            dataInstance = await mainRepository.acquirePostData(req);
             res.status(200).send(dataInstance);
         }
         catch (err) {
