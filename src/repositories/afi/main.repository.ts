c61bc50b60bd8678c718b13822a25b5f1a40aede
George Miller <133229709+gmm2204@users.noreply.github.com>
2023-09-07 20:54:53 +0300

    Big update [07092023] (#22)
    * * Implemented post method for filter routes for mortality_ncov
    * Implemented conditional param on routes for filters
    * Implemented filters queries
    
    * Emergency bcp
    
    * Implemented filters query for some routes for mortality_ncov module
    * Implemented routes have the filter property set to true in src/data/mortality_ncov/routes.json
    
    * Added fillters to Mortality and SARI/ILI routes
    
    * Implemented changes proposed on 05092023
    
    * Implement more updates from review 15092023
    
    * * Implemented filters logic for AFI module (Pending updated queries)
    * Implemented filters logic for SARI/ILI (Pending updated queries -> not all routes have updated queries)
    * Updated AFI and SARI/ILI routes to receive post requests

--- src/repositories/afi/main.repository.ts
+++ src/repositories/afi/main.repository.ts
@@ -1,7 +1,9 @@
+import { Request, Response } from "express";
 import { QueryTypes } from "sequelize";
 import Database from "../../db/database";
 import * as RoutesData from '../../data/afi/routes.json';
 import { config } from "../../config/db.config";
+import { DataFilter } from "../../models/DataFilter.model";
 
 interface IMainRepository {
     readData(url: string): Promise<any[]>;
@@ -10,6 +12,81 @@ interface IMainRepository {
 class MainRepository implements IMainRepository {
     db = new Database(config.DB_AFI);
     private retrievedData: any;
+
+    async readPostData(req: Request): Promise<any[]> {
+        let url = req.url;
+        let targetRoute: {} = {};
+        let route_query: string = ``;
+        let route_filter_query: string = ``;
+        let route_found: boolean = false;
+        let filterInstance = new DataFilter(req.body);
+
+        //#region Compile filters
+        if (filterInstance.filter_facility != "-1") {
+            if (route_filter_query != ``) {
+                route_filter_query += ' AND '
+            }
+
+            // A.FacilityID
+            route_filter_query += ` A.FacilityID = '` + filterInstance.filter_facility + `' `;
+        }
+
+        if ((filterInstance.filter_date_range_start != "-1") && (filterInstance.filter_date_range_end != "-1")) {
+            if (route_filter_query != ``) {
+                route_filter_query += ' AND '
+            }
+
+            // D.Date
+            route_filter_query += ` (D.Date BETWEEN '` + filterInstance.filter_date_range_start + `' AND '` + filterInstance.filter_date_range_end + `')`;
+        }
+
+        if (filterInstance.filter_year != "-1") {
+            if (route_filter_query != ``) {
+                route_filter_query += ' AND '
+            }
+
+            // D.Year
+            route_filter_query += ` D.Year = '` + filterInstance.filter_year + `' `;
+        }
+
+        if ((filterInstance.filter_epi_week_start != "-1") && (filterInstance.filter_epi_week_end != "-1")) {
+            if (route_filter_query != ``) {
+                route_filter_query += ' AND '
+            }
+
+            // A.EpiWeek
+            route_filter_query += ` (A.EpiWeek BETWEEN ` + filterInstance.filter_epi_week_start + ` AND ` + filterInstance.filter_epi_week_end + `) `;
+        }
+
+        if (route_filter_query != ``) {
+            route_filter_query = ` WHERE (` + route_filter_query + `) `;
+        }
+        //#endregion
+
+        RoutesData.routes.forEach(seekRoute => {
+            if (seekRoute.url == url) {
+                targetRoute = seekRoute;
+                route_found = true;
+                route_query = seekRoute.query;
+
+                if (seekRoute.filter) {
+                    route_query = route_query.replace("{{WHERE}}", route_filter_query);
+                }
+            }
+        });
+
+        if ((route_found) && (route_query != "")) {
+            this.retrievedData = await this.db.sequelize?.query<any[]>(route_query, {
+                type: QueryTypes.SELECT,
+            });
+        } else {
+            this.retrievedData = [{
+                "message": "Unhandled request --> " + url
+            }];
+        }
+
+        return this.retrievedData;
+    }
 
     async readData(url: string): Promise<any[]> {
         let route_query: string = ``;
