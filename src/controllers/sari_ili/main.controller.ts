d9e1d3706f3bef486e84aae08b8db313156d26c6
George Miller <133229709+gmm2204@users.noreply.github.com>
2023-09-11 09:49:16 +0300

    Update main.controller.ts

--- src/controllers/sari_ili/main.controller.ts
+++ src/controllers/sari_ili/main.controller.ts
@@ -2,19 +2,6 @@ import { Request, Response } from "express";
 import mainRepository from "../../repositories/sari_ili/main.repository";
 
 export default class MainController {
-    async getData(req: Request, res: Response) {
-        try {
-            let dataInstance: any;
-            dataInstance = await mainRepository.readData(req.url);
-            res.status(200).send(dataInstance);
-        }
-        catch (err) {
-            res.status(500).send({
-                message: "An error has occured here --> " + req.url
-            });
-        }
-    }
-
     async postData(req: Request, res: Response) {
         try {
             let dataInstance: any;
